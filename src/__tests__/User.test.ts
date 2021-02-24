a54b823755b32c3a9b24e209c9dd4f5230ace69b
Lucas Gabriel <lucasgab.lggs@gmail.com>
2021-02-24 13:23:17 -0300

    Aula 03 - NLW 04 - 24/02/2021

---
+++ src/__tests__/User.test.ts
@@ -0,0 +1,29 @@
+import { response } from 'express';
+import request from 'supertest';
+import { app } from "../app";
+import createConnection from '../database'
+
+describe("User", () => {
+    beforeAll(async () => {
+        const connection = await createConnection();
+        await connection.runMigrations();
+    });
+
+    it("Should be able to create a new user", async () => {
+        const response = await request(app).post("/users").send({
+            email: "user@example.com",
+            name: "User Example"
+        });
+
+        expect(response.status).toBe(201);
+    });
+
+    it ("Should not be able to create a new existant user", async () => {
+        const response = await request(app).post("/users").send({
+            email: "user@example.com",
+            name: "User Example"
+        });
+
+        expect(response.status).toBe(400);
+    });
+});
--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,12 +1,12 @@
-import { Request, Response } from 'express';
-import { getRepository } from 'typeorm';
-import { User } from '../models/User';
+import { Request, Response } from "express";
+import { getCustomRepository } from "typeorm";
+import { UsersRepository } from "../repositories/UsersRepository";
 
 class UserController {
     async create(request: Request, response: Response){
         const { name, email } = request.body;
 
-        const usersRepository = getRepository(User);
+        const usersRepository = getCustomRepository(UsersRepository);
 
         const userAlreadyExists = await usersRepository.findOne({
             email
@@ -24,7 +24,7 @@ class UserController {
 
         await usersRepository.save(user);
 
-        return response.json(user);
+        return response.status(201).json(user);
     }
 }
 
